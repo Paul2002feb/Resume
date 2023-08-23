f6fb886aaa69411c414aeff11d893886260abb00
Paul Kiattikhunphan <ppk6chf@virginia.edu>
2023-08-23 19:56:19 -0400

    added colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
             </div>
             <div className="flex w-1/2 flex-col items-center self-center lg:w-full lg:text-center">
               <AnimatedText
-                text="Bridging Imagination & Technology."
+                text="Bridging Imagination & Technology!"
                 className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               {/* <p className="mt-0 ml-0 mb-2 text-base font-medium md:text-sm sm:text-xs text-primary dark:text-primaryDark">
